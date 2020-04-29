403ab64fa324aa0aea624dd95fb457d82369ddaf
Dat Adithya <naruita201@gmail.com>
2020-04-29 12:14:53 +0530

    Added in sets

---
+++ sets.py
@@ -0,0 +1,6 @@
+set1 = {1, 2, 3, 4, 5}
+set2 = {2, 3, 4, 6, 7, 8}
+print("The union of two sets : ", set1|set2)
+print("The intersection of two sets : ", end="")
+print(set1 & set2)
+print("Subtraction of set1 from set 2", set1-set2)
