a8e245db1bb497f1a6baae6e4acfb5b129edf812
Dat Adithya <naruita201@gmail.com>
2020-05-09 06:09:43 +0530

    Updated to insert via user input

--- psql/db_connect.py
+++ psql/db_connect.py
@@ -1,11 +1,28 @@
 import psycopg2
 
-#connecting to psql
-conn = psycopg2.connect(dbname='postgres', user='postgres', password='test123', port='5432')
-cursor = conn.cursor()
+def create():
+    #connecting to psql
+    conn = psycopg2.connect(dbname='postgres', user='postgres', password='test123', port='5432')
+    cursor = conn.cursor()
 
-cursor.execute('''CREATE TABLE student(NAME TEXT, AGE INTEGER, ADDRESS TEXT);''')
+    cursor.execute('''CREATE TABLE student(NAME TEXT, AGE INTEGER, ADDRESS TEXT);''')
 
-print('Table created.')
-conn.commit()
-conn.close()
+    print('Table created.')
+    conn.commit()
+    conn.close()
+
+def insert_data():
+    conn = psycopg2.connect(dbname='postgres', user='postgres', password='test123', port='5432')
+    cursor = conn.cursor()
+
+    name = input("Enter the name : ")
+    age = int(input("Enter the age : "))
+    address = input("Enter the address : ")
+    query = '''INSERT INTO student (NAME, AGE, ADDRESS) VALUES (%s, %s, %s);'''
+    cursor.execute(query, (name, age, address))
+
+    print('Inserted.')
+    conn.commit()
+    conn.close()
+
+insert_data()
