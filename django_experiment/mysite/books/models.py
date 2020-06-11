4461f1121a4dfade7813ea987bc60b99e95d1d90
dat-adi <naruita201@gmail.com>
2020-06-11 14:44:21 +0530

    Added in template for HTML

--- django_experiment/mysite/books/models.py
+++ django_experiment/mysite/books/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+
+    def __str__(self):
+        return self.name + '-' + self.author + ' ' + self.type + ' ' + self.price
+
+    name = models.CharField(max_length=100)
+    author = models.CharField(max_length=40)
+    price = models.CharField(max_length=30)
+    type = models.CharField(max_length=50)
+
--- django_experiment/mysite/books/views.py
+++ django_experiment/mysite/books/views.py
@@ -1,8 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
+from .models import Book
+from django.template import loader
 # Create your views here.
 
 
 def index(request):
-    return HttpResponse("<h>A homepage for the website</h>")
+    all_books = Book.objects.all()
+    template = loader.get_template('books/index.html')
+    context = {
+        'all_books' : all_books
+    }
+
+    return HttpResponse(template.render(context, request))
+
+
+def detail(request, book_id):
+    return HttpResponse("<h2> Details for Book ID : " + str(book_id) + "</h2>")
