97c1be1df5894605a9454c8b16e0f57784707317
dat-adi <naruita201@gmail.com>
2020-06-12 19:09:16 +0530

    Used generic views to increase readability.

--- django_experiment/mysite/books/views.py
+++ django_experiment/mysite/books/views.py
@@ -1,21 +1,14 @@
-from django.shortcuts import render
-from django.http import HttpResponse, Http404
+from django.views import generic
 from .models import Book
-from django.template import loader
 
 
-def index(request):
-    all_books = Book.objects.all()
-    template = loader.get_template('books/index.html')
-    context = {
-        'all_books' : all_books
-    }
-    return render(request, 'books/index.html', context)
+class IndexView(generic.ListView):
+    template_name = 'books/index.html'
+
+    def get_queryset(self):
+        return Book.objects.all()
 
 
-def detail(request, book_id):
-    try:
-        book = Book.objects.get(id=book_id)
-    except Book.DoesNotExist:
-        raise Http404('Hey, I don\'t think that exists?')
-    return render(request, 'books/detail.html', {'book': book})
+class DetailView(generic.DetailView):
+    model = Book
+    template_name = 'books/detail.html'
