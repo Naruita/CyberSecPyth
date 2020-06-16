65bbbdfdcfca80d005a85166ae111675bf8debaa
dat-adi <naruita201@gmail.com>
2020-06-16 17:35:31 +0530

    Used username in html

--- flasksite/app.py
+++ flasksite/app.py
@@ -2,9 +2,9 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-	return render_template('hello.html')
+@app.route('/welcome/<username>')
+def index(username):
+	return render_template('hello.html', name=username)
 
 @app.route('/success/<name>')
 def success(name):
